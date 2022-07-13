3485b16fcf0afde431ded6cfe4ea4e8d45870b68
maxmarvell <maximilianmarvell@dyn3167-116.wlan.ic.ac.uk>
2022-07-13 12:08:50 +0100

    Changes to color of plots and swapping radiative power for conventional

--- Pth.py
+++ Pth.py
@@ -12,30 +12,32 @@ conventional_600 = df.loc[(df['Loop current [kA]'].values==600)]
 conventional_750 = df.loc[(df['Loop current [kA]'].values==750)]
 superX = df.loc[(df['Divertor'].values=='SUPER-X')]
 
-for div in [conventional_600,conventional_750,superX]:
+colors = ['midnightblue', 'royalblue','orangered']
+
+for div, col in zip([conventional_600,conventional_750,superX],colors):
     plt.figure(1)
-    plt.errorbar(div['ne E-20'].dropna().values,div['Pth [MW]'].dropna().values,yerr=div['SDPth [MW]'].dropna().values,fmt='^')
-    plt.errorbar(div['ne E-20.1'].dropna().values,div['Pth [MW].1'].dropna().values,yerr=div['SDPth [MW].2'].dropna().values,fmt='v')
+    plt.errorbar(div['ne E-20'].dropna().values,div['Pth [MW]'].dropna().values,yerr=div['SDPth [MW]'].dropna().values,fmt='^',color=col)
+    plt.errorbar(div['ne E-20.1'].dropna().values,div['Pth [MW].1'].dropna().values,yerr=div['SDPth [MW].2'].dropna().values,fmt='v',color=col)
 
     plt.figure(2)
-    plt.errorbar(div['ne E-20'].dropna().values,div['Pth no rad [MW]'].dropna().values,yerr=div['SDPth [MW].1'].dropna().values,fmt='^')
-    plt.errorbar(div['ne E-20.1'].dropna().values,div['Pth no rad [MW].1'].dropna().values,yerr=div['SDPth [MW].3'].dropna().values,fmt='v')
+    plt.errorbar(div['ne E-20'].dropna().values,div['Pth no rad [MW]'].dropna().values,yerr=div['SDPth [MW].1'].dropna().values,fmt='^',color=col)
+    plt.errorbar(div['ne E-20.1'].dropna().values,div['Pth no rad [MW].1'].dropna().values,yerr=div['SDPth [MW].3'].dropna().values,fmt='v',color=col)
 
     plt.figure(3)
-    plt.errorbar(div['ne E-20'].dropna().values,div['Pth [MW]'].dropna().values,yerr=div['SDPth [MW]'].dropna().values,fmt='^')
+    plt.errorbar(div['ne E-20'].dropna().values,div['Pth [MW]'].dropna().values,yerr=div['SDPth [MW]'].dropna().values,fmt='^',color=col)
 
     plt.figure(4)
-    plt.errorbar(div['ne E-20'].dropna().values,div['Pth no rad [MW]'].dropna().values,yerr=div['SDPth [MW].1'].dropna().values,fmt='^')
+    plt.errorbar(div['ne E-20'].dropna().values,div['Pth no rad [MW]'].dropna().values,yerr=div['SDPth [MW].1'].dropna().values,fmt='^',color=col)
 
 plt.figure(1)
 plt.ylabel('Pth [MW]')
 plt.xlabel('ne E-20 []')
-plt.legend(['LH conventional - 600kA','LH conventional - 750kA','LH SuperX - 600kA','HL conventional at 600kA','HL conventional - 750kA','HL SuperX - 600kA'])
+plt.legend(['LH conventional - 600kA','HL conventional at 600kA','LH conventional - 750kA','HL conventional - 750kA','LH SuperX - 600kA','HL SuperX - 600kA'])
 
 plt.figure(2)
 plt.ylabel('Pth no rad [MW]')
 plt.xlabel('ne E-20 []')
-plt.legend(['LH conventional - 600kA','LH conventional - 750kA','LH SuperX - 600kA','HL conventional at 600kA','HL conventional - 750kA','HL SuperX - 600kA'])
+plt.legend(['LH conventional - 600kA','HL conventional at 600kA','LH conventional - 750kA','HL conventional - 750kA','LH SuperX - 600kA','HL SuperX - 600kA'])
 
 plt.figure(3)
 plt.ylabel('Pth [MW]')
